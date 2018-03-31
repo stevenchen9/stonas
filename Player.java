effddd44edd6275d0959bb5c08e16d7549c7231f
Steven Chen <stevenchen@brandeis.edu>
2018-03-30 23:31:33 -0400

    Edit Player
    Added some details

--- Player.java
+++ Player.java
@@ -3,6 +3,15 @@ public class Player {
 	
 	// Name of the player
 	private String name;
+	private int goodness;
 	
-
+	public Player(String name) {
+		this.name = name;
+		goodness = 0;
+	}
+	
+	// return the name of the player
+	public String getName() {
+		return this.name;
+	}
 }
