d11416efcb3491fdfa000e2b2e754c13b6931662
Jonas Tjahjadi <jonastjahjadi@gmail.com>
2018-04-01 12:20:20 -0400

    more backend methods / loops from online

--- com/stonas/main/Game.java
+++ com/stonas/main/Game.java
@@ -1,6 +1,9 @@
 package com.stonas.main;
 
 import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
 
 public class Game extends Canvas implements Runnable {
 	
@@ -33,7 +36,54 @@ public class Game extends Canvas implements Runnable {
 	}
 	
 	public void run() {
+		// POPULAR GAME LOOP
+		long lastTime = System.nanoTime();
+		double amountOfTicks = 60.0;
+		double ns = 1000000000 / amountOfTicks;
+		double delta = 0;
+		long timer = System.currentTimeMillis();
+		int frames = 0;
+		while(running) {
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			lastTime = now;
+			while(delta >= 1) {
+				tick();
+				delta--;
+			}
+			
+			if(running)
+				render();
+			frames++;
+			
+			if(System.currentTimeMillis() - timer > 1000) {
+				timer += 1000;
+				System.out.println("FPS: " + frames);
+				frames = 0;
+			}
+		}
+		stop();
+	}
+	
+	private void tick() {
 		
+	}
+	
+	private void render() {
+		BufferStrategy bs = this.getBufferStrategy();
+		if(bs == null) {
+			// don't go over 3
+			this.createBufferStrategy(3);
+			return;
+		}
+		
+		Graphics g = bs.getDrawGraphics();
+		
+		g.setColor(Color.black);
+		g.fillRect(0,  0,  WIDTH, HEIGHT);
+		
+		g.dispose();
+		bs.show();
 	}
 	
 	public static void main(String[] args) {
